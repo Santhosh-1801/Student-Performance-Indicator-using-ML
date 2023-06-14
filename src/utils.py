2142f77d109e295d321dc9195153fb4c7b83351e
Santhosh-1801 <santhoshhodge@gmail.com>
2023-06-14 13:36:28 +0530

    Minor Changes

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 import os 
 import sys 
+import pickle
 
 import pandas as pd 
 import numpy as np 
@@ -45,5 +46,12 @@ def evaluate_models(X_train,Y_train,X_test,Y_test,models,param):
  
 
         return report 
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path,"rb") as file_obj:
+            return pickle.load(file_obj)
     except Exception as e:
         raise CustomException(e,sys)
